deb5fa7c6ffa7c0e9ad748ac03e714859b0a32c0
MezemovRufetCode <rufatam@code.edu.az>
2021-10-03 23:07:16 +0400

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Include count of array elements:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Array elements:");
+            int[] Myarr =new int[n];        
+            for(int i = 0; i < n; i++)
+            {
+                Myarr[i]=int.Parse(Console.ReadLine());
+            }
+            Result r1 = new Result(Myarr);
+            r1.Find(Myarr);
+        }
+    }
+}
---
+++ Task/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task
+{
+    class Result
+    {
+        public int[] Arr { get; set; }
+
+        public Result(int[] arr)
+        {
+            Arr = arr;
+        }
+        public void Find(int[] Arr1)
+        {
+
+            int pcount=0,ncount=0,zcount=0;
+
+            for(int i = 0; i < Arr1.Length; i++)
+            {
+                if (Arr1[i] == 0)
+                {
+                    zcount++;
+                }
+                else
+                if (Arr1[i] > 0)
+                {
+                    pcount++;
+                }
+                else
+                    ncount++;
+            }
+            Console.WriteLine($"Count of Positive digits:{pcount}\nCount of Negative digits:{ncount}\nCount of Zero:{zcount}"); 
+        }
+    }
+}
